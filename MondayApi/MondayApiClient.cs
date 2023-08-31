8e7cea269df83170b008e15163b508b1542189cb
░▒▓█│【Walkman】│█▓▒░ <matthewc@direwolf.co.za>
2023-08-31 12:47:21 +0200

    added GraphQL.Client package

--- MondayApi/MondayApiClient.cs
+++ MondayApi/MondayApiClient.cs
@@ -1,6 +1,17 @@
-﻿namespace MondayApi {
+﻿using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+
+namespace MondayApi {
     public class MondayApiClient : IMondayApiClient {
+        private readonly string baseURL = "https://api.monday.com/v2";
+        private readonly GraphQLHttpClient client;
+
         public MondayApiClient(string token) {
+            client = new GraphQLHttpClient(baseURL, new NewtonsoftJsonSerializer());
+
+            client.HttpClient.DefaultRequestHeaders.Add("Authorization", token);
+            client.HttpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.HttpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
         }
     }
 }
