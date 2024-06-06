e01edba608320a8981a6b2bb274e9613eed0360e
░▒▓█│【Walkman】│█▓▒░ <matthewc@direwolf.co.za>
2024-06-06 13:58:37 +0200

    Added WebhookResponse classes

---
+++ MondayApi/WebhookResponses/MondayEvents.cs
@@ -0,0 +1,22 @@
+namespace MondayApi.WebhookResponses {
+    public class MondayChallenge {
+        public string Challenge { get; set; }
+    }
+
+    public class MondayEventBase : MondayChallenge {
+        public EventClassBase Event { get; set; }
+    }
+
+    public class MondayItemCreated : MondayChallenge {
+        public EventClassItemCreated Event { get; set; }
+    }
+    public class MondayItemNameChanged : MondayChallenge {
+        public EventClassItemNameChanged Event { get; set; }
+    }
+    public class MondayItemGroupChanged : MondayChallenge {
+        public EventClassItemGroupChanged Event { get; set; }
+    }
+    public class MondayColumnChanged : MondayChallenge {
+        public EventClassColumnChanged Event { get; set; }
+    }
+}
