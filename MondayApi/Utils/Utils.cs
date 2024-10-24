DireWolfZA/MondayApiClient

commit d50572bb4e7340dea6e9563b69934061c6fc95f4

added some info to README, changed PeopleValueForUpdate to not use custom entity - PeopleEntity already uses a Kind enum that has lowercase JSON representations

MondayApi/Utils/Utils.cs
@@ -33,7 +33,7 @@ namespace MondayApi.Utils {
                     return nv.Number ?? nv.Text ?? nv.Value;
                 case PeopleValue pv:
                     return new PeopleValueForUpdate() {
-                        PersonsAndTeams = pv.PersonsAndTeams?.Select(pe => new PeopleEntityForUpdate() { ID = pe.ID, Kind = pe.Kind.ToString().ToLowerInvariant() }).ToList(),
+                        PersonsAndTeams = pv.PersonsAndTeams,
                     };
                 case BoardRelationValue bv:
                     if (bv is BoardRelationValueForUpdate)

MondayApi/Schema/Models/ColumnValues/PeopleValue.cs
@@ -18,10 +18,6 @@ namespace MondayApi.Schema {
 
     internal class PeopleValueForUpdate {
         [JsonProperty("personsAndTeams")] // see https://developer.monday.com/api-reference/docs/people#json
-        public ICollection<PeopleEntityForUpdate> PersonsAndTeams { get; set; }
-    }
-    internal class PeopleEntityForUpdate {
-        public string ID { get; set; }
-        public string Kind { get; set; }
+        public ICollection<PeopleEntity> PersonsAndTeams { get; set; }
     }
 }

MondayApi/Utils/DebugSerializer.cs
@@ -13,16 +13,11 @@ namespace MondayApi.Utils {
         readonly NewtonsoftJsonSerializer implementation = new NewtonsoftJsonSerializer();
         readonly Action<string> captureResponse;
 
-        public DebugSerializer(Action<string> captureResponse) =>
-            this.captureResponse = captureResponse;
-        public byte[] SerializeToBytes(GraphQLWebSocketRequest request) =>
-            implementation.SerializeToBytes(request);
-        public Task<WebsocketMessageWrapper> DeserializeToWebsocketResponseWrapperAsync(Stream stream) =>
-            implementation.DeserializeToWebsocketResponseWrapperAsync(stream);
-        public GraphQLWebSocketResponse<TResponse> DeserializeToWebsocketResponse<TResponse>(byte[] bytes) =>
-            implementation.DeserializeToWebsocketResponse<TResponse>(bytes);
-        public string SerializeToString(GraphQLRequest request) =>
-            implementation.SerializeToString(request);
+        public DebugSerializer(Action<string> captureResponse) => this.captureResponse = captureResponse;
+        public byte[] SerializeToBytes(GraphQLWebSocketRequest request) => implementation.SerializeToBytes(request);
+        public Task<WebsocketMessageWrapper> DeserializeToWebsocketResponseWrapperAsync(Stream stream) => implementation.DeserializeToWebsocketResponseWrapperAsync(stream);
+        public GraphQLWebSocketResponse<TResponse> DeserializeToWebsocketResponse<TResponse>(byte[] bytes) => implementation.DeserializeToWebsocketResponse<TResponse>(bytes);
+        public string SerializeToString(GraphQLRequest request) => implementation.SerializeToString(request);
 
         public async Task<GraphQLResponse<TResponse>> DeserializeFromUtf8StreamAsync<TResponse>(Stream stream, CancellationToken cancellationToken) {
             using (var ms = new MemoryStream()) {

MondayApi/WebhookResponses/EventClasses.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 
+//https://developer.monday.com/api-reference/reference/webhooks#sample-payload-for-webhook-events
 namespace MondayApi.WebhookResponses {
     public class EventClassBase {
         public string App { get; set; }

MondayApi/generate api schema.cmd
@@ -1,8 +1,8 @@
 @cd %~dp0
 graphql-client-generator --serviceUrl "https://api.monday.com/v2" ^
---outputPath .\Schema ^
---namespace "MondayApi.Schema" ^
---outputType OneClassPerFile ^
---idTypeMapping String ^
---header "Authorization:%1" ^
---header "API-Version: 2024-04"
+  --outputPath .\Schema ^
+  --namespace "MondayApi.Schema" ^
+  --outputType OneClassPerFile ^
+  --idTypeMapping String ^
+  --header "Authorization:%1" ^
+  --header "API-Version: 2024-04"

commit d182aa311965c6da07881a9f6ed10c671448afca

fixed updating PeopleValue column caused by d50572bb - SerializeColumnValue(s) uses custom JsonSerializerSettings that ignores EnumMember attributes - now has special handling for Enums

MondayApi/Utils/Utils.cs
@@ -17,12 +17,12 @@ namespace MondayApi.Utils {
         public static GraphQlQueryParameter<T> GetParameter<T>(T value, bool isNullable = true) =>
             value == null ? null : new GraphQlQueryParameter<T>(null, defaultValue: value, isNullable);
 
+        private static readonly Newtonsoft.Json.Serialization.SnakeCaseNamingStrategy snakeCaseNamingStrategy = new Newtonsoft.Json.Serialization.SnakeCaseNamingStrategy();
         private static readonly Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings() {
-            ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver() {
-                NamingStrategy = new Newtonsoft.Json.Serialization.SnakeCaseNamingStrategy()
-            },
-            NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore
+            ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver() { NamingStrategy = snakeCaseNamingStrategy },
+            NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
         };
+        static Utils() => settings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter() { NamingStrategy = snakeCaseNamingStrategy });
 
         private static object convertColumn(IColumnValue column) {
             column.ID = null;