6cce0721b1c625fe66e6abae626f893e00858664
░▒▓█│【Walkman】│█▓▒░ <matthewc@direwolf.co.za>
2025-02-04 09:41:31 +0200

    added Date property to DateValue DebuggerDisplay

--- MondayApi/Schema/Models/ColumnValues/DateValue.cs
+++ MondayApi/Schema/Models/ColumnValues/DateValue.cs
@@ -2,7 +2,7 @@ using System;
 using Newtonsoft.Json;
 
 namespace MondayApi.Schema {
-    [System.Diagnostics.DebuggerDisplay("{Type.ToString(),nq}:{ID}: {Text}")]
+    [System.Diagnostics.DebuggerDisplay("{Type.ToString(),nq}:{ID}: {Text}/{Date}")]
     [GraphQlObjectType("DateValue")]
     public class DateValue : IMirroredValue, IColumnValue {
         public Column Column { get; set; }
