2505d479b8c28ce617a7c6477326673db9f4354f
░▒▓█│【Walkman】│█▓▒░ <matthewc@direwolf.co.za>
2025-05-02 23:39:59 +0200

    prepare for using nullable reference types

--- MondayApi/generate api schema.cmd
+++ MondayApi/generate api schema.cmd
@@ -3,6 +3,8 @@ graphql-client-generator --serviceUrl "https://api.monday.com/v2" ^
   --outputPath .\Schema ^
   --namespace "MondayApi.Schema" ^
   --outputType OneClassPerFile ^
-  --idTypeMapping String ^
+  --idTypeMapping string ^
+  --csharpVersion CSharp6 ^
+  --nullableReferences true ^
   --header "Authorization:%1" ^
   --header "API-Version: 2024-10"
