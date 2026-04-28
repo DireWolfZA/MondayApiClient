b9bee945937f669b16965e4517460797f62b6ac1
░▒▓█│【Walkman】│█▓▒░ <matthewc@direwolf.co.za>
2026-04-28 08:52:45 +0200

    added LastUpdated column UpdaterID value to Items queries

--- MondayApi/Items/ItemActions.cs
+++ MondayApi/Items/ItemActions.cs
@@ -26,6 +26,7 @@ namespace MondayApi.Items {
                         .WithCheckboxValueFragment(new CheckboxValueQueryBuilder().WithChecked())
                         .WithPhoneValueFragment(new PhoneValueQueryBuilder().WithCountryShortName())
                         .WithEmailValueFragment(new EmailValueQueryBuilder().WithEmail())
+                        .WithLastUpdatedValueFragment(new LastUpdatedValueQueryBuilder().WithUpdaterID())
                         .WithMirrorValueFragment(new MirrorValueQueryBuilder().WithDisplayValue())
                     ,
                     ids: Utils.Utils.GetParameter(columnIDs)
